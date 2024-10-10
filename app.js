dcc5e215e1133f4fb01d8511268e80a590511b3e
thungan610 <156633890+thungan610@users.noreply.github.com>
2024-10-10 13:27:39 +0700

    kết nối mongo

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+//require mongoose
+const mongoose = require("mongoose");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -18,9 +19,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors()); //cho phép gọi api từ các domain khác nhau
+
+// kết nối mongo
+mongoose.connect("mongodb://localhost:27017/DATN") // tên dự án
+.then(() => console.log("Connected to MongoDB..."))
+.catch(() => console.log("Could not connect to MongoDB..."));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
