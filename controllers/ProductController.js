6a151321b18cad638a3f286fe6042992ca4eeebf
ThanTuanPhatne <thantuanphat294@gmail.com>
2024-10-12 19:47:31 +0700

    apiProductDetail&Top10Selling

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -19,4 +19,36 @@ const getProduct_App = async () => {
         throw new Error('Lấy danh sách sản phẩm lỗi');
     }
 }
-module.exports = {getProduct_App}
+
+//Lấy chi tiết sản phẩm
+
+const getProductDetailById_App = async (id) => {
+    try {
+        const productInDB = await ProductModel.findById(id);
+        if (!productInDB) {
+            throw new Error("Không tìm thấy sp");
+        }
+        return productInDB;
+    } catch (error) {
+        console.log('getProducts error: ', error.message);
+        throw new Error('Lấy danh sách sản phẩm lỗi');
+    }
+}
+
+// thống kê top 10 sp bán chạy nhiều nhất 
+const getTopProductSell_App = async () => {
+    try {
+      let query = {};
+      const products = await ProductModel.find(query, " name, sold")
+        .sort({ sold: -1 })
+        .limit(10);
+        
+      return products;
+    } catch (error) {
+      console.log("getTopProduct error: ", error.massage);
+      throw new Error("Lấy sp lỗi");
+    }
+  };
+  
+ 
+module.exports = {getProduct_App, getProductDetailById_App, getTopProductSell_App }
