0461eeeac03eff45aa19bbee48d64d8b8c7b2df4
thungan610 <156633890+thungan610@users.noreply.github.com>
2024-10-13 13:52:20 +0700

    search

--- routes/products.js
+++ routes/products.js
@@ -28,16 +28,31 @@ router.get('/getProductDetailById_App/:id', async(req, res, next) => {
 
 // Lấy top 10 sp bán chạy nhất trong app
 
-router.get('/getTopProductSell_App', async(req, res, next) => {
-      try {
-        const products = await ProductController.getTopProductSell_App(name,sold);
+router.get('/getTopProductSell_', async (req, res, next) => {
+    try {
+      
+       const products = await ProductController.getTopProductSell_App();
+       return res.status(200).json({ status: true, data: products });
+    } catch (error) {
+       return res.status(500).json({ status: false, data: error.message });
+    }
+ });
+ 
+//  __________________________________searech____________________________________--
+//http://localhost:6677/products/search?key=coca
+// Tìm kiến sản phẩm theo từ khóa (SEARCH)
+router.get('/search', async (req, res, next) => {
+    try {
+        const { key } = req.query;
+        // console.log('dfshdjbvhd', key);
+        const products = await ProductController.findProducts_App(key);
+        console.log(products);
         return res.status(200).json({ status: true, data: products });
     } catch (error) {
+        console.log('Tìm kiếm sản phẩm thất bại');
         return res.status(500).json({ status: false, data: error.message });
     }
-});
-
-
+})
 
 
 module.exports = router;
