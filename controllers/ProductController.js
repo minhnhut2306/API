8dfcfc3f6d20040403cab8b2b8d28187ebc0922a
thungan610 <156633890+thungan610@users.noreply.github.com>
2024-10-13 14:10:55 +0700

    lượt bán

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -36,10 +36,10 @@ const getProductDetailById_App = async (id) => {
 }
 
 // thống kê top 10 sp bán chạy nhiều nhất 
-const getTopProductSell_App = async () => {
+const getTopProductSell_Web = async () => {
     try {
       let query = {};
-      const products = await ProductModel.find(query, " name, sold")
+      const products = await ProductModel.find(query, ' name sold')
         .sort({ sold: -1 })
         .limit(10);
         
@@ -50,24 +50,48 @@ const getTopProductSell_App = async () => {
     }
   };
    
+  
 //   _________________search___________________
-const findProducts_App = async (key) => {
+
+// =================================================================================================================
+
+// const findProducts_App = async (key) => {
+//     try {
+//         let query = {};
+
+//         query = {
+//             ...query,
+//             name: { $regex: key, $options: 'i' }
+//         }
+
+//         const products = await ProductModel
+//             .find(query,'name,price,images,uom')
+//         // .sort({ name: -1 });
+//         return products;
+
+//     } catch (error) {
+//         console.log('findProduct error: ', error.message);
+//         throw new Error('Tìm kiếm sản phẩm không thành công');
+//     }
+// 
+
+// =================================================================================================================
+const findProductsByKey_App = async (key) => {
     try {
-        let query = {};
-
-        query = {
-            ...query,
-            name: { $regex: key, $options: 'i' }
-        }
-
+        // Tạo câu truy vấn tìm kiếm
+        let query = {
+            name: { $regex: key, $options: 'i' } // Tìm kiếm tên sản phẩm theo từ khóa (không phân biệt hoa thường)
+        };
+        
+        // Chỉ định các trường cần lấy ra từ cơ sở dữ liệu
         const products = await ProductModel
-            .find(query,'name,price,images,uom')
-        // .sort({ name: -1 });
+            .find(query)
+            .select('name price image oum'); // Chỉ lấy các trường name, price, image, và oum
+        
         return products;
-
     } catch (error) {
-        console.log('findProduct error: ', error.message);
-        throw new Error('Tìm kiếm sản phẩm không thành công');
+        console.log('getProducts error: ', error.message);
+        throw new Error('Lấy danh sách sản phẩm lỗi');
     }
 }
-module.exports = {getProduct_App, getProductDetailById_App, getTopProductSell_App,findProducts_App }
+module.exports = {getProduct_App, getProductDetailById_App, getTopProductSell_Web,findProductsByKey_App }
