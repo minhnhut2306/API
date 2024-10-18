ea89365bd4467259b5ff76bfdbd6a1f204f9f514
thungan610 <156633890+thungan610@users.noreply.github.com>
2024-10-18 14:08:12 +0700

    update

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,11 +1,11 @@
 const { isValidObjectId } = require('mongoose');
 const ProductModel = require('./ProductModel');
-
+const CategoryModel = require('./CategoryModel');
 //________________________________________APP_______________________________________
 
 //Lấy danh sách sản phẩm (HOME)
 
-const getProduct_App = async () => {
+const getProduct = async () => {
     try {
         let query = {};
         query = {
@@ -40,21 +40,21 @@ const getProductDetailById_App = async (id) => {
 // thống kê top 10 sp bán chạy nhiều nhất 
 const getTopProductSell_Web = async () => {
     try {
-      let query = {};
-      const products = await ProductModel.find(query, ' name sold')
-        .sort({ sold: -1 })
-        .limit(10);
-        
-      return products;
-    } catch (error) {
-      console.log("getTopProduct error: ", error.massage);
-      throw new Error("Lấy sp lỗi");
-    }
-  };
-  // =================================================================================================================
+        let query = {};
+        const products = await ProductModel.find(query, ' name sold')
+            .sort({ sold: -1 })
+            .limit(10);
 
-   
-  
+        return products;
+    } catch (error) {
+        console.log("getTopProduct error: ", error.massage);
+        throw new Error("Lấy sp lỗi");
+    }
+};
+// =================================================================================================================
+
+
+
 //   _________________search___________________
 
 // =================================================================================================================
@@ -86,43 +86,43 @@ const findProductsByKey_App = async (key) => {
         let query = {
             name: { $regex: key, $options: 'i' } // Tìm kiếm tên sản phẩm theo từ khóa (không phân biệt hoa thường)
         };
-        
+
         // Chỉ định các trường cần lấy ra từ cơ sở dữ liệu
         const products = await ProductModel
             .find(query)
             .select('name price image oum'); // Chỉ lấy các trường name, price, image, và oum
-        
+
         return products;
     } catch (error) {
         console.log('getProducts error: ', error.message);
         throw new Error('Lấy danh sách sản phẩm lỗi');
     }
-<<<<<<< HEAD
-} ;
+    // <<<<<<< HEAD
+};
 
 // xóa sản phẩm 
 // Xóa sp theo id
 const deleteProduct = async (id) => {
-   
+
     try {
-      // tìm sản phẩm theo id
-   
-      const productInDB = await ProductModel.findById(id);
-    
-      if (!productInDB) {
-        throw new Error("Sản phẩm không tồn tại");
-      }
-      //Xóa sp
-      await ProductModel.deleteOne({ _id: id });
-      return true;
+        // tìm sản phẩm theo id
+
+        const productInDB = await ProductModel.findById(id);
+
+        if (!productInDB) {
+            throw new Error("Sản phẩm không tồn tại");
+        }
+        //Xóa sp
+        await ProductModel.deleteOne({ _id: id });
+        return true;
     } catch (error) {
-      console.log("deleteProduct: ", error);
-      throw new Error("Xóa sp lỗi");
+        console.log("deleteProduct: ", error);
+        throw new Error("Xóa sp lỗi");
     }
-  };
-module.exports = {getProduct_App, getProductDetailById_App, getTopProductSell_Web,findProductsByKey_App, deleteProduct }
-=======
-}
+};
+
+// =======
+// }
 // =================================================================================================================
 const addProduct = async (name, price, quantity, images, description) => {
     try {
@@ -142,10 +142,10 @@ const addProduct = async (name, price, quantity, images, description) => {
 
         // Tạo một sản phẩm mới từ đối tượng product
         const newProduct = new ProductModel(product);
-        
+
         // Lưu sản phẩm vào cơ sở dữ liệu
         const result = await newProduct.save();
-        
+
         return result; // Trả về sản phẩm mới đã được lưu
     } catch (error) {
         console.error('addProduct error: ', error.message);
@@ -153,5 +153,43 @@ const addProduct = async (name, price, quantity, images, description) => {
     }
 };
 
-module.exports = {getProduct_App, getProductDetailById_App, getTopProductSell_Web,findProductsByKey_App,addProduct }
->>>>>>> branhNgan
+// sửa 
+const updateProduct = async (id, name, price, quantity, images, description, category) => {
+    try {
+        // tìm sản phẩm theo id 
+        console.log(id)
+        const udtProduct = await ProductModel.findById(id);
+        if (!udtProduct) {
+            throw new Error("Sản phẩm không tồn tại");
+
+        }
+        if (!category) {
+            throw new Error("Vui lòng chọn danh mục");
+        }
+        // lấy category theo id 
+        const udtcCategory = await CategoryModel.findById(category);
+        if (!udtcCategory) {
+            throw new Error("Danh mục không tồn tại");
+        }
+        // tạo object category
+        category = {
+            category_id: udtcCategory._id,
+            category_name: udtcCategory.name
+        }
+        udtProduct.name = name || udtcCategory.name;
+        udtProduct.price = price || udtProduct.price;
+        udtProduct.quantity = quantity || udtProduct.quantity;
+        udtProduct.images = images || udtProduct.images;
+        udtProduct.description = description || udtProduct.description;
+        udtProduct.category = category || udtProduct.category;
+        udtProduct.updateProduct=Date.now;
+
+        await udtProduct.save();
+        return true;
+    } catch {
+        console.log('updateProduct error: ', error.message);
+        throw new Error('Cập nhập sản phẩm lỗi');
+    }
+}
+module.exports = { getProduct, getProductDetailById_App, getTopProductSell_Web, findProductsByKey_App, addProduct,updateProduct }
+
--- routes/products.js
+++ routes/products.js
@@ -5,9 +5,9 @@ const ProductController = require('../controllers/ProductController');
 
 // ____________________________Lấy sp home_______________________
 // http://localhost:6677/products/getProducts_App
-router.get('/getProducts_App', async (req, res) => {
+router.get('/getProducts', async (req, res) => {
     try {
-        const products = await ProductController.getProduct_App();
+        const products = await ProductController.getProduct();
         return res.status(200).json({ status: true, data: products });
     } catch (error) {
         return res.status(500).json({ status: false, data: error.message });
@@ -78,16 +78,33 @@ router.delete('/:id/delete', async (req, res, next ) => {
 
 router.post('/addSP', [validation.validateProduct], async (req, res, next) => {
     try {
-        const { name, price, quantity, images, description,  } = req.body;
-        const product = await ProductController.addProduct(name, price, quantity, images, description, );
+        const { name, price, quantity, images, description, category } = req.body;
+        const product = await ProductController.addProduct(name, price, quantity, images, description, category);
+        return res.status(200).json({ status: true, data: product });
+    } catch (error) {
+        return res.status(500).json({ status: false, data: error.message });
+    }
+});
+
+
+
+
+///////////////////////
+
+// update sp 
+
+
+router.put('/:id/update', [validation.validateProduct], async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name, price, quantity, images, description, category } = req.body;
+        console.log('---------->' + req.body + "   " + category)
+        const product = await ProductController.updateProduct(id, name, price, quantity, images, description, category);
         return res.status(200).json({ status: true, data: product });
     } catch (error) {
         return res.status(500).json({ status: false, data: error.message });
     }
 })
 
-
-
-//
 
 module.exports = router;
