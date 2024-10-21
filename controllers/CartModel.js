f56aff70c0e1195514b4e8e527e83cb9f44a9e97
ThanTuanPhatne <thantuanphat294@gmail.com>
2024-10-21 20:16:36 +0700

    get3monthsago

--- controllers/CartModel.js
+++ controllers/CartModel.js
@@ -8,12 +8,14 @@ const CartSchema = new Schema({
     //id, name
     user: {type: Object, require: true},
     total: {type: Number, default: 0},
+    address: {type: Object, require: true},
     //{id, name, price, quantity}
     products: {type: Array, default: []},
     //1 xác nhận, 2: đang giao, 3: hoàn thành, 4: hủy
     status: {type: Number, default: AppConstants.CART_STATUS.XAC_NHAN},
     //ngày giờ mua
     date: {type: Date, default: Date.now},
+
 })
 
 module.exports = mongoose.models.cart || mongoose.model('cart', CartSchema);
