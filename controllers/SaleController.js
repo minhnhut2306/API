76bb0bda06516d527dc255669a7224a81b9296d4
ThanTuanPhatne <thantuanphat294@gmail.com>
2024-10-23 14:34:08 +0700

    updateSale

--- controllers/SaleController.js
+++ controllers/SaleController.js
@@ -1,47 +1,44 @@
 const { model } = require("mongoose");
 const SaleModel = require("./SaleModel");
 
+const getSale = async () => {
+  try {
+    let query = {};
+    const sale = await SaleModel.find(query);
+    return sale;
+  } catch (error) {
+    console.error("Sale error: ", error.message);
+    throw new Error("Lấy khuyến mãi thất bại: " + error.message);
+  }
+};
 
-
-const getSale = async() => {
-    try {
-        let query = {}
-        const sale = await SaleModel.find(query);
-        return sale;
-    } catch (error) {
-        console.error("Sale error: ", error.message);
-        throw new Error("Lấy khuyến mãi thất bại: " + error.message);
+const getDetailSale = async (id) => {
+  try {
+    const saleInDB = await SaleModel.findById(id);
+    if (!saleInDB) {
+      throw new Error("Không tìm thấy khuyến mãi");
     }
-}
+    return saleInDB;
+  } catch (error) {
+    console.error("Sale error: ", error.message);
+    throw new Error("Lấy chi tiết khuyến mãi thất bại: " + error.message);
+  }
+};
 
-const getDetailSale = async(id) =>{
-    try {
-        const saleInDB = await SaleModel.findById(id);
-        if (!saleInDB) {
-            throw new Error("Không tìm thấy khuyến mãi");
-        }
-        return saleInDB;
-    } catch (error) {
-        console.error("Sale error: ", error.message);
-        throw new Error("Lấy chi tiết khuyến mãi thất bại: " + error.message);
+const deleteSale = async (id) => {
+  try {
+    const saleInDB = await SaleModel.findById(id);
+    if (!saleInDB) {
+      throw new Error("Không tìm thấy khuyến mãi");
     }
-}
+    await SaleModel.deleteOne({ _id: id });
+    return true;
+  } catch (error) {
+    console.error("Sale error: ", error.message);
+    throw new Error("Xóa khuyến mãi thất bại: " + error.message);
+  }
+};
 
-const deleteSale = async(id) =>{
-    try {
-        const saleInDB = await SaleModel.findById(id);
-        if (!saleInDB) {
-            throw new Error("Không tìm thấy khuyến mãi");
-        }
-        await SaleModel.deleteOne({ _id: id})
-        return true;
-    } catch (error) {
-        console.error("Sale error: ", error.message);
-        throw new Error("Xóa khuyến mãi thất bại: " + error.message);
-    }
-}
-
-// add
 // add
 const addSale = async (
   date,
@@ -84,6 +81,45 @@ const addSale = async (
     throw new Error("Thêm khuyến mãi thất bại: " + error.message);
   }
 };
+
+const updateSale = async (
+  date,
+  title,
+  discountAmount,
+  discountPercent,
+  minOrderValue,
+  expirationDate,
+  isExpired,
+) => {
+  try {
+  
+    const saleInDB = await SaleModel.findById(id);
+
+    if (!saleInDB) {
+      throw new Error("Khôngtìm thấy khuyến mãi");
+    }
+
+    saleInDB.date = date || saleInDB.date;
+    saleInDB.title = title || saleInDB.title;
+    saleInDB.discountAmount = discountAmount || saleInDB.discountAmount;
+    saleInDB.discountPercent = discountPercent || saleInDB.discountPercent;
+    saleInDB.minOrderValue = minOrderValue || saleInDB.minOrderValue;
+    saleInDB.expirationDate = expirationDate || saleInDB.expirationDate;
+    saleInDB.isExpired = isExpired || saleInDB.isExpired;
+
+    saleInDB.updateSale = Date().now;
+
+    await saleInDB.save();
+    return saleInDB;
+  } catch (error) {
+    console.error("Sale error: ", error.message);
+    throw new Error("Sửa khuyến mãi thất bại: " + error.message);
+  }
+};
 module.exports = {
-  addSale, getSale, getDetailSale, deleteSale,
+  addSale,
+  getSale,
+  getDetailSale,
+  deleteSale,
+  updateSale,
 };
