e19c8840ba341bdc42a220e4f0c18bfc4bd3fde4
thungan610 <156633890+thungan610@users.noreply.github.com>
2024-10-25 14:54:03 +0700

    thông báo & address

--- routes/notification.js
+++ routes/notification.js
@@ -4,14 +4,24 @@ const NotificationController = require('../controllers/NotificationController')
 
 // lấy thông báo 
 
-router.get('/', async (req, res, next )=> {
+// router.get('/', async (req, res, next )=> {
+//     try {
+//         const categories = await NotificationController.getCategories();
+//         return res.status(200).json({ status: true, data: categories});
+//     } catch (error) {
+//         console.log('Get categories error: ', error.massage);
+//         return res.status(500).json({ status: false, data: error.massage});
+//     }
+// });
+router.post('/add_notification', async (req, res, next) => {
     try {
-        const categories = await NotificationController.getCategories();
-        return res.status(200).json({ status: true, data: categories});
+        const { product, sale } = req.body;
+        const result = await NotificationController.addNotification(product, sale);
+        return res.status(200).json({ status: true, data: result });
+
     } catch (error) {
-        console.log('Get categories error: ', error.massage);
-        return res.status(500).json({ status: false, data: error.massage});
+        console.log(error.message);
+        return res.status(500).json({ status: false, data: error.message });
     }
 });
-
 module.exports = router;
