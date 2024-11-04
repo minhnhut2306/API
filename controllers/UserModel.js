38b7c53480bd60f7c08cc786fe711507a700bbe7
ngocphat922004 <130296645+ngocphat922004@users.noreply.github.com>
2024-11-04 22:33:34 +0700

    Reapply "Merge branch 'main' into Branch_cphat"
    This reverts commit 94efdd3432680a58cc73f5c9999cc6116d60aaf1.

--- controllers/UserModel.js
+++ controllers/UserModel.js
@@ -1,31 +1,28 @@
 // khai báo 1 schema cho users
 // (_id, email, password, name, role, carts, createdAt, updatedAt, available)
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const UserSchema = new Schema({
-    email: { type: String, required: true, unique: true }, // required (dữ liệu cần truyền vô)
-    password: { type: String, required: true },
-    name: { type: String, required: true },
-    phone:{type: String, required: true},
-    address:{type: Array,  default: [ ]},
-    bio:{type: String,default: ''}, // tiểu sử
-    gender:{type: String, default: ''},
-    birthday:{type: String, required:false}, 
-
-
-    //lịch sử mua hàng
-    
-    //{id, date, total, status}
-    carts: { type: Array, default: [ ] },
-    // ngày giờ tạo
-    createdAt: { type: Date, default: Date.now },
-    // ngày giờ cập nhật
-    updatedAt: { type: Date, default: Date.now },
-    // tài khoản còn hoạt động hay không
-    available: { type: Boolean, default: true },
-    // giỏ hàng tạm
+  email: { type: String, required: true, unique: true }, // required (dữ liệu cần truyền vô)
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: Array, default: [] },
+  bio: { type: String, default: "" }, // tiểu sử
+  gender: { type: String, default: "" },
+  birthday: { type: String, required: false },
+  //lịch sử mua hàng
+  //{id, date, total, status}
+  carts: { type: Array, default: [] },
+  // ngày giờ tạo
+  createdAt: { type: Date, default: Date.now },
+  // ngày giờ cập nhật
+  updatedAt: { type: Date, default: Date.now },
+  // tài khoản còn hoạt động hay không
+  available: { type: Boolean, default: true },
+  // giỏ hàng tạm
 });
 // tiếng anh, số ít, chữ thường, không dấu, không cách
-module.exports = mongoose.models.user || mongoose.model('user', UserSchema);
+module.exports = mongoose.models.user || mongoose.model("user", UserSchema);
