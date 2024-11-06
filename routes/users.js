c7a1ee20d858e3b15316fb2facbc44f1aa546dfa
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-11-06 23:47:07 +0700

    Dinh Nhan Sửa lại code User bị thiếu

--- routes/users.js
+++ routes/users.js
@@ -145,4 +145,15 @@ try {
     return res.status(500).json({ status: false, data: error.message });
 }
 });
+
+router.get("/:id/getProfileApp", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await userController.getProfile(id);
+    return res.status(200).json({ status: true, data: result });
+  } catch (error) {
+    console.log("Get NewUsers error:", error.message);
+    return res.status(500).json({ status: false, data: error.message });
+  }
+});
 module.exports = router;
