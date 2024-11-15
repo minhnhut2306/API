54ca9a7fe5e34658d069231f22f56fbd36164c3d
ngocphat922004 <130296645+ngocphat922004@users.noreply.github.com>
2024-11-15 14:08:10 +0700

    .

--- controllers/AddressModel.js
+++ controllers/AddressModel.js
@@ -5,27 +5,20 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const AddressSchema = new Schema({
+  user: {
+      userId: {type: Schema.Types.ObjectId, 
+        ref: 'users',
+        required: true,},
+      name: { type: String, required: true }, 
+      phone: { type: String, required: true },
+  },
+  houseNumber: { type: String, required: true },
+  alley: { type: String, required: true },
+  quarter: { type: String, required: true },
+  district: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  available: { type: Boolean, default: true }
+}, { timestamps: true });
 
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",required: true,
-      },
-
-    user: {type: Object, require: true},
-    houseNumber: { type: String, required: true },
-    alley: { type: String, required: true },
-    quarter: { type: String, required: true },
-    district: { type: String, required: true },
-    city: { type: String, required: true },
-    country: { type: String, required: true },
-    // ngày giờ tạo
-    createdAt: { type: Date, default: Date.now },
-    // ngày giờ cập nhật
-    updatedAt: { type: Date, default: Date.now },
-    // tài khoản còn hoạt động hay không
-    available: { type: Boolean, default: true },
-    // giỏ hàng tạm
-
-});
-// tiếng anh, số ít, chữ thường, không dấu, không cách
 module.exports = AddressSchema;
